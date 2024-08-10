2e258572546d52341ba80268ade2fe23f9ed7e67
Sujan pal <palsujan50@gmail.com>
2024-08-10 15:09:06 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+
+//"33" => 33
+//"33abc" =>NaN
+//true => 1; false =>0
+
+// let isLoggedIn = 1
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn)
+
+let somenum= 33
+let stringnumber = String(somenum)
+console.log(stringnumber)
+console.log(typeof stringnumber);
+
+
+
+
