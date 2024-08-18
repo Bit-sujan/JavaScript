a8bed2240510fdcbb50612550c80ed115c141225
Sujan pal <176762831+Bit-sujan@users.noreply.github.com>
2024-08-18 19:10:07 +0000

    some dates's method

---
+++ 01_basics/datesIn.js
@@ -0,0 +1,37 @@
+// Dates...
+
+let myyDate = new Date()
+// console.log(myyDate);
+// console.log( myyDate.toString());
+// console.log(myyDate.toDateString());
+// console.log(myyDate.toLocaleString());
+// console.log(typeof myyDate);
+
+
+// let mynewDate = new Date()
+let mynewDate = new Date(2024, 0, 25)
+// let mynewDate = new Date(2024, 0, 25, 5, 6)
+// let mynewDate = new Date("2024-01-25")
+//console.log(mynewDate.toLocaleString());
+
+
+let myyTimeStamp = Date.now()
+// console.log(myyTimeStamp);
+// console.log(mynewDate.getTime()); // for exact value
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `//  
+// `` >> string intropolation
+
+
+// newDate.toLocaleString(' default',{
+//     weekday : "long",
+//     timeZone :''
+// })
+
+
