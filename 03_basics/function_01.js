1d83dcc431e4c420fa4521c74804164c002b147d
Sujan pal <176762831+Bit-sujan@users.noreply.github.com>
2024-08-23 10:19:48 +0000

    create some basics functions

---
+++ 03_basics/function_01.js
@@ -0,0 +1,41 @@
+// FUNCTIONS >>>
+
+function myname(){
+    console.log("Sujan ");
+    console.log("Pal");
+    
+// }
+//myname // >> this is functions referance
+myname() // this is function call
+
+function addTwoNmubers(num1 , num2){
+    console.log(num1+ num2);
+}
+function addTwoNmubers(num1 , num2){
+    // let result =num1+ num2
+    // return result;
+    return num1 + num2s
+}
+ const result = addTwoNmubers(20,40)
+console.log("result : ",result);
+
+
+// function username(user){
+//     if(user === undefined){
+//         console.log("please enter a user name..");
+//         return
+//     }
+//     return `${user} just logged in `
+
+}
+console.log(username())
+
+function username(user ="ram"){
+    if(!user ){
+        console.log("please enter a user name..");
+        return
+    }
+    return `${user} just logged in `
+
+}
+console.log(username("sujan")) // its can override the previous value
