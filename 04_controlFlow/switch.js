a319a67d85039e0d1dbdf36434daa23fa2b51232
Sujan pal <176762831+Bit-sujan@users.noreply.github.com>
2024-09-09 19:12:29 +0000

    example of switch statement

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,22 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("this is jan");
+     break;    
+    case 2:
+        console.log("this is feb");
+        break;
+    case 3:
+        console.log("this is march");
+        break;
+    case 4:
+        console.log("this is april");
+        
+        break;
+
+    default:
+        console.log("defult user");
+        
+        break;
+}
