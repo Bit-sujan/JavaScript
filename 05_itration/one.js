0854706d93a1fe8f56f66350158bc4bdf1f065e0
Sujan pal <176762831+Bit-sujan@users.noreply.github.com>
2024-09-10 18:44:35 +0000

    for loop with break and continue....

---
+++ 05_itration/one.js
@@ -0,0 +1,35 @@
+// for 
+
+//  for (let index = 0; index < 5; index++) {
+//     const element = index;
+// //     console.log(element);
+    
+    
+//  }
+
+
+for (let i = 1; i<=10;i++){
+    //console.log(`number: ${i}`); 
+    for( let j =1; j<= 10; j++){
+        //console.log(i+ '*' + j + '=' + i*j);
+        
+    }
+    
+}
+
+// let MyArray = [ "rohit", "virat" , "Dhoni"]
+// for (let index = 0; index < MyArray.length; index++) {
+//     const element = MyArray[index];
+//     //console.log(element);
+// }
+
+// Break and continue
+
+for( let index = 1; index <= 20; index ++){
+    if(index = 5){
+        console.log(`detected 5`);
+        //continue
+        break
+    }
+console.log(`value of i is ${index}`);
+}
