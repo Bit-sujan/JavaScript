7d4f350f79821392840ff1fd88b4b0254e695bb4
Sujan pal <176762831+Bit-sujan@users.noreply.github.com>
2024-09-14 18:46:28 +0000

    basic code of while loop & do while loop

---
+++ 05_itration/two.js
@@ -0,0 +1,23 @@
+// while loop
+let i = 0
+while (i<=10) {
+    console.log(`value of i is ${i}`);
+    i = i+2
+    
+}
+
+let myArray = [" dhoni", "virat", "rohit"]
+ let a = 0
+while (a < myArray.length) {
+    console.log(`player name is ${myArray[a]}`);
+    a++;
+}
+
+// do-while loop
+
+goal =0
+do{
+    console.log(`goal no ${goal}`);
+    goal++
+
+}while (goal<=10)
